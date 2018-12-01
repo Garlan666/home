10358390367568720915a50f9f3b31104a84640a
Garlan666 <1572279033@qq.com>
2018-12-01 11:43:42 +0800

    del

--- js/map.js
+++
@@ -1,7 +0,0 @@
-var map = new BMap.Map("map");
-map.enableScrollWheelZoom();
-var p=new BMap.Point(113.086486, 22.593605);
-map.centerAndZoom(p, 11);
-map.addControl(new BMap.NavigationControl());
-var marker=new BMap.Marker(p);
-map.addOverlay(marker);
